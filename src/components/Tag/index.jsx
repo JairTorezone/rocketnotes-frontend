94d42a8247f3858649f32ea5209e1683f58d0a7c
Jair Torezone <jairtorezone@gmail.com>
2024-01-22 20:52:22 -0300

    Project rocketnotes - frontend

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,19 @@
+import { Container } from "./styles";
+
+export function Tag({ title, ...rest }){
+
+return (
+
+<Container {...rest}>
+
+
+{title}
+
+
+</Container>
+
+
+);
+
+
+}
---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { Profile } from "../pages/Profile";
+import { Details } from "../pages/Details";
+import { Home } from "../pages/Home";
+import { New } from "../pages/New";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/new" element={<New />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
